35f930dff25c36c7a7b503df7c2cbc599bbd08bc
zafnet <mrobm33@gmail.com>
2025-01-27 21:57:00 -0300

    Add emojis hyprland.conf moxi.lua saxs.lua maps.lua coment al lado Mod rofi nvcut mejor vista

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -1,100 +1,66 @@
--- Mapeos De Nvim
-local map = vim.keymap.set
+local map = vim.keymap.set -- Variable Local Para Mapeos
 
-vim.g.mapleader = ' '
+vim.g.mapleader = ' '      -- Seteando Tecla Lider
 
-map('n', '<c-s>', ':w<CR>', { noremap = true })
-map('n', '<c-x>', ':q!<CR>', { noremap = true })
-map('n', '<A-q>', ':q<CR>', { noremap = true })
+map('n', '<c-s>', ':w<CR>', { noremap = true }) -- Guarda El archivo actual
+map('n', '<c-x>', ':q!<CR>', { noremap = true })-- Sale sin Guarda Forzado El archivo actual
+map('n', '<A-q>', ':q<CR>', { noremap = true }) -- Sale sin Guarda El archivo actual
 
--- Crea Comentario Con Espacio Para Escribir Luego
-map('n', '<leader>·', 'i#<space>', { noremap = true })
-map('n', '<leader>"', 'i"<space>', { noremap = true })
+map('n', '<leader>y', 'i#<space><Esc>', { noremap = true }) -- comentario # Con Espacio Atras
+map('n', '<leader>m', 'i"<space><Esc>', { noremap = true }) -- comentario " Con Espacio Atras
 
--- Crea Comentario Con Espacio Atras De Algo Escrito
-map('n', '<leader>y', 'i#<space><Esc>', { noremap = true })
-map('n', '<leader>m', 'i"<space><Esc>', { noremap = true })
+map('n', 'đ', ':FZF<CR>', { noremap = true })  -- Usar FZF En NVIM Con La Tecla altgr + f
+map('n', '<leader>f', '/', { noremap = true }) -- Colocar La Barra Para Buscar
+map('n', '<leader>bf', ':buffers<CR>', { noremap = true }) -- Ver Los Buffers Abiertos
 
--- Usar FZF En NVIM Con La Tecla altgr + f
-map('n', 'đ', ':FZF<CR>', { noremap = true })
+map('n', '<leader>o', 'o<Esc>', { noremap = true })        -- Agrega Un Espacio En Blanco hacia abajo y Pasas Al Modo Insertar
+map('n', '<leader>p', 'O<Esc>', { noremap = true })        -- Agrega Un Espacio En Blanco hacia arriba y Pasas Al Modo Insertar
+map('n', '<leader>k', 'i<space><Esc>', { noremap = true }) -- Agrega Un Espacio En Blanco hacia la derecha
 
--- Colocar La Barra Para Buscar
-map('n', '<leader>f', '/', { noremap = true })
+map('n', '<leader>c', ':e $MYVIMRC<CR>', { noremap = true }) -- LLama Al Archivo De Configuracion De Nvim
 
--- Ver Los Buffers Abiertos
-map('n', '<leader>bf', ':buffers<CR>', { noremap = true })
+map('n', '<c-t>', ':split<CR>:term<CR>:resize 15<CR>:startinsert<CR>', { noremap = true }) -- Crea Terminal En Horizontal En Modo Normal
+map('t', '<Esc><Esc>', '<C-\\><C-n>', { noremap = true })  -- Sale De Terminal va a Nvim cambia de ventana Con A+h,l,k,j C-x quita ventana de Terminal
 
--- Agrega Un Espacio En Blanco y Pasas Al Modo Insertar
-map('n', '<leader>o', 'o<Esc>', { noremap = true })
-map('n', '<leader>p', 'O<Esc>', { noremap = true })
-map('n', '<leader>k', 'i<space><Esc>', { noremap = true })
+map('n', '<leader>h', '<C-w>s', { noremap = true }) -- Crear ventana Horizontal Del Mismo Buffer
+map('n', '<leader>v', '<C-w>v', { noremap = true }) -- Crear ventana Vertical Del Mismo Buffer
 
--- LLama Al Archivo De Configuracion De Nvim
-map('n', '<leader>c', ':e $MYVIMRC<CR>', { noremap = true })
+map('n', '<leader>vv', '<C-w>t<C-w>H', { noremap = true }) -- Cambia ventana En Vertical Del Mismo Buffer Cuando Estan Creadas
+map('n', '<leader>vh', '<C-w>t<C-w>K', { noremap = true }) -- Cambia ventana En Horizontal Del Mismo Buffer Cuando Estan Creadas
 
--- Crea Terminal En Horizontal En Modo Normal
-map('n', '<c-t>', ':split<CR>:term<CR>:resize 15<CR>:startinsert<CR>', { noremap = true })
+map('n', '<leader>bn', ':bn<CR>', { noremap = true })                  -- Se Mueve Al Buffer Siguiente
+map('n', '<leader>bp', ':bp<CR>', { noremap = true })                  -- Se Mueve Al Buffer Anterior
+map('n', '<leader>d', ':bd<CR>', { noremap = true })                   -- Elimina El Buffer Actual
+map('n', '<leader>s', ':%s/', { noremap = true }) -- Sustituye Palabra En Todo El Buffer
 
--- Sale De La Terminal Para Poder Ir A La Ventana De Nvim Con A-{h,l,k,j} C-x Elimina La Terminal
-map('t', '<Esc><Esc>', '<C-\\><C-n>', { noremap = true })
+map('n', '<A-h>', '<C-w>h', { noremap = true }) -- Mueve El Cursor a La ventana  Derecha
+map('n', '<A-l>', '<C-w>l', { noremap = true }) -- Mueve El Cursor a La ventana Izquierda
+map('n', '<A-j>', '<C-w>j', { noremap = true }) -- Mueve El Cursor a La ventana Abajo
+map('n', '<A-k>', '<C-w>k', { noremap = true }) -- Mueve El Cursor a La ventana Arriba
 
--- Se Mueve Al Buffer Anterior O Posterior
-map('n', '<leader>bn', ':bn<CR>', { noremap = true })
-map('n', '<leader>bp', ':bp<CR>', { noremap = true })
+map('n', 'Y', 'yg$', { noremap = true })           -- Copia Una Linea Mostrando un Resaltado
+map('n', '<C-ñ>', ':nohl<CR>', { noremap = true }) -- Quita Resaltado De Busqueda
 
--- Elimina El Buffer Actual
-map('n', '<leader>d', ':bd<CR>', { noremap = true })
+map('n', '<leader>q', 'q:', { noremap = true }) -- Abre Ventana Con Historial De Comandos Que Se Usaron
 
--- Sustituye Palabra En Todo El Buffer
-map('n', '<leader>s', ':%s///g<left><left><left>', { noremap = true })
+map('n', '<C-l>', '<C-w>L', { noremap = true }) -- Mueve Las Ventanas C-l Derecha
+map('n', '<C-h>', '<C-w>H', { noremap = true }) -- Mueve Las Ventanas C-h Izquierda
+map('n', '<C-j>', '<C-w>J', { noremap = true }) -- Mueve Las Ventanas C-j Abajo
+map('n', '<C-k>', '<C-w>K', { noremap = true }) -- Mueve Las Ventanas C-k Arriba
 
--- Mueve El Cursor a La ventana Arriba Abajo Izquierda o Derecha
-map('n', '<A-h>', '<C-w>h', { noremap = true })
-map('n', '<A-l>', '<C-w>l', { noremap = true })
-map('n', '<A-j>', '<C-w>j', { noremap = true })
-map('n', '<A-k>', '<C-w>k', { noremap = true })
+map('n', '<leader>r', ':so %<CR>', { noremap = true, silent = true }) -- Recargar Archivo Actual
 
--- Copia Una Linea Mostrando un Resaltado
-map('n', 'Y', 'yg$', { noremap = true })
+map('i', '((', '()<left>', { noremap = true })     -- Inserta Simbolos De  () En Modo Insertar
+map('i', '{{', '{}<left>', { noremap = true })     -- Inserta Simbolos De  {} En Modo Insertar
+map('i', '[[', '[]<left>', { noremap = true })     -- Inserta Simbolos De  [] En Modo Insertar
+map('i', '\'\'', '\'\'<left>', { noremap = true }) -- Inserta Simbolos De  '' En Modo Insertar
+map('i', '""', '""<left>', { noremap = true })     -- Inserta Simbolos De  "" En Modo Insertar
 
--- Quita Resaltado De Busqueda
-map('n', '<C-ñ>', ':nohl<CR>', { noremap = true })
+map('n', '<leader>z', '<C-w>_ |<C-w>|', { noremap = true }) -- +Zoom a La Ventana Actual
+map('n', '<leader>x', '<C-w>=', { noremap = true })         -- -Zoom a La Ventana Actual
 
--- Cambia ventana En Vertical y Horizontal Del Mismo Buffer Cuando Estan Creadas
-map('n', '<leader>vv', '<C-w>t<C-w>H', { noremap = true })
-map('n', '<leader>vh', '<C-w>t<C-w>K', { noremap = true })
+map('n', '<A-C-h>', ':vertical resize +10<CR>', { noremap = true, silent = true }) -- Tamaño De Ventana Vertical De +10
+map('n', '<A-C-l>', ':vertical resize -10<CR>', { noremap = true, silent = true }) -- Tamaño De Ventana Vertical De -10
 
--- Crear ventana Horizontal y Vertical Del Mismo Buffer
-map('n', '<leader>h', '<C-w>s', { noremap = true })
-map('n', '<leader>v', '<C-w>v', { noremap = true })
-
--- Abre Ventana Con Historial De Comandos Que Se Usaron
-map('n', '<leader>q', 'q:', { noremap = true })
-
--- Mueve Las Ventanas C-l Derecha C-h Izquierda C-j Abajo C-k Arriba
-map('n', '<C-l>', '<C-w>L', { noremap = true })
-map('n', '<C-h>', '<C-w>H', { noremap = true })
-map('n', '<C-j>', '<C-w>J', { noremap = true })
-map('n', '<C-k>', '<C-w>K', { noremap = true })
-
--- Recargar Archivo Actual
-map('n', '<leader>r', ':so %<CR>', { noremap = true, silent = true })
-
--- Inserta Simbolos De  () {} [] "" '' En Modo Insertar
-map('i', '((', '()<left>', { noremap = true })
-map('i', '{{', '{}<left>', { noremap = true })
-map('i', '[[', '[]<left>', { noremap = true })
-map('i', '\'\'', '\'\'<left>', { noremap = true })
-map('i', '""', '""<left>', { noremap = true })
-
--- +Zoom y -Zoom a La Ventana Actual
-map('n', '<leader>z', '<C-w>_ |<C-w>|', { noremap = true })
-map('n', '<leader>x', '<C-w>=', { noremap = true })
-
--- Tamaño De Ventana Vertical De +10 y -10
-map('n', '<A-C-h>', ':vertical resize +10<CR>', { noremap = true, silent = true })
-map('n', '<A-C-l>', ':vertical resize -10<CR>', { noremap = true, silent = true })
-
--- Tamaño De Ventana Hacia Arriba Y Abajo De +10 y -10
-map('n', '<A-C-k>', ':resize +10<CR>', { noremap = true, silent = true })
-map('n', '<A-C-j>', ':resize -10<CR>', { noremap = true, silent = true })
+map('n', '<A-C-k>', ':resize +10<CR>', { noremap = true, silent = true }) -- Tamaño De Ventana Hacia Arriba Y Abajo De +10
+map('n', '<A-C-j>', ':resize -10<CR>', { noremap = true, silent = true }) -- Tamaño De Ventana Hacia Arriba Y Abajo De -10
--- .config/rofi/menuatajos/nvcut/nvcut.sh
+++ .config/rofi/menuatajos/nvcut/nvcut.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
-/bin/bash -c "$(cat ~/.config/nvim/lua/maps.lua | rofi -show -dmenu -i -p "Maps de Nvim" -theme ~/.config/rofi/menuatajos/nvcut/nvcut.rasi|sort)"
+# Filtrar y formatear los alias con sus descripciones
+cat ~/.config/nvim/lua/maps.lua | \
+awk '
+    # Ignorar líneas que no contienen mapeos
+    /^local map = vim.keymap.set/ { next }
+    /^vim.g.mapleader =/ { next }
+
+    # Procesar líneas que contienen mapeos
+    {
+        # Extraer el maps y la descripción
+        match($0, /map\(([^,]+),\s*([^,]+),\s*([^,]+),/, arr); 
+        map_name = arr[2]; 
+        description = gensub(/.*--/, "", "g", $0); 
+        if (map_name != "") {
+            print map_name, description; 
+        }
+    }
+' | \
+rofi -show -dmenu -i -p "Maps de Nvim lua" -theme ~/.config/rofi/menuatajos/nvcut/nvcut.rasi | sort
