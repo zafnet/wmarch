8000f5b7bbb2d3ec46833c43bb1b80aa186f5845
zafnet <mrobm33@gmail.com>
2025-01-28 19:09:21 -0300

    r x R en init.lua Del bashc en hdot

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -16,26 +16,25 @@ local current_mode = {
   ["\22"] = 'vb', -- <C-v> se representa como \22 en Lua
   i  = 'i',
   R  = 'r',
-  Rv = 'rv',
   c  = 'c',
   t  = 'T'
 }
 
 -- Definir colores y estilos para la Barra de estado lado Izquierdo
-vim.api.nvim_set_hl(0, 'NormalColor', { bg = '#000000', fg = '#00af00', bold = true })     -- '#000000' -> 0, '#00af00' -> 34
-vim.api.nvim_set_hl(0, 'InsertColor', { bg = '#000000', fg = '#ffff00', bold = true })     -- '#ffff00' -> 226
-vim.api.nvim_set_hl(0, 'VisualColor', { bg = '#000000', fg = '#ff8700', bold = true })     -- '#ff8700' -> 214
-vim.api.nvim_set_hl(0, 'VlColor', { bg = '#ff5f00', fg = '#000000', bold = true })         -- '#ff5f00' -> 202, '#000000' -> 0
-vim.api.nvim_set_hl(0, 'VbColor', { bg = '#ff005f', fg = '#000000', bold = true })         -- '#ff005f' -> 197
-vim.api.nvim_set_hl(0, 'ReplaceColor', { bg = '#000000', fg = '#ff00ff', bold = true })    -- '#ff00ff' -> 201
-vim.api.nvim_set_hl(0, 'TerminalColor', { bg = '#000000', fg = '#ff005f', bold = true })   -- '#ff005f' -> 197
-vim.api.nvim_set_hl(0, 'CommandColor', { bg = '#000000', fg = '#5faf00', bold = true })    -- '#5faf00' -> 70
+vim.api.nvim_set_hl(0, 'NormalColor', { bg = '#000000', fg = '#00af00', bold = true })
+vim.api.nvim_set_hl(0, 'InsertColor', { bg = '#000000', fg = '#ffff00', bold = true })
+vim.api.nvim_set_hl(0, 'VisualColor', { bg = '#000000', fg = '#ff8700', bold = true })
+vim.api.nvim_set_hl(0, 'VlColor', { bg = '#ff5f00', fg = '#000000', bold = true })
+vim.api.nvim_set_hl(0, 'VbColor', { bg = '#ff005f', fg = '#000000', bold = true })
+vim.api.nvim_set_hl(0, 'ReplaceColor', { bg = '#000000', fg = '#ff00ff', bold = true })
+vim.api.nvim_set_hl(0, 'TerminalColor', { bg = '#000000', fg = '#ff005f', bold = true })
+vim.api.nvim_set_hl(0, 'CommandColor', { bg = '#000000', fg = '#5faf00', bold = true })
 
 -- Colores Para La Barra De Estado lado Derecho
-vim.api.nvim_set_hl(0, 'rutadarch', { bg = '#626262', fg = '#00d75f' })                    -- '#626262' -> 240, '#00d75f' -> 42
-vim.api.nvim_set_hl(0, 'ln', { bg = '#626262', fg = '#00af00' })                           -- '#00af00' -> 34
-vim.api.nvim_set_hl(0, 'co', { bg = '#626262', fg = '#ffffff' })                           -- '#ffffff' -> 15
-vim.api.nvim_set_hl(0, 'buf', { bg = '#626262', fg = '#ffaf00' })                          -- '#ffaf00' -> 214
+vim.api.nvim_set_hl(0, 'rutadarch', { bg = '#626262', fg = '#00d75f' })
+vim.api.nvim_set_hl(0, 'ln', { bg = '#626262', fg = '#00af00' })
+vim.api.nvim_set_hl(0, 'co', { bg = '#626262', fg = '#ffffff' })
+vim.api.nvim_set_hl(0, 'buf', { bg = '#626262', fg = '#ffaf00' })
 
 -- Definir la función en el entorno global de Neovim
 _G.get_statusline = function()
@@ -46,7 +45,7 @@ _G.get_statusline = function()
     statusline = statusline .. "%#NormalColor#  NORMAL "
   elseif mode == 'i' then
     statusline = statusline .. "%#InsertColor#  INSERT "
-  elseif mode == 'r' then
+  elseif mode == 'R' then
     statusline = statusline .. "%#ReplaceColor#  REPLACE "
   elseif mode == 'v' then
     statusline = statusline .. "%#VisualColor#  VISUAL "
